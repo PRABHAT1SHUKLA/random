8e03028339d8884d7dad5e2546d3903bc484d7be
prabhat <prabhatshukla0849@gmail.com>
2024-10-10 21:47:51 +0530

    order placed endpoint

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ let INR_BALANCES ={
   user2: { balance: 20000, locked: 5000 },
 };
 
-let ORDERBOOK = {
+let ORDERBOOK : any= {
   BTC_USDT_10_Oct_2024_9_30: {
     yes: {
       '9.5': { total: 12, orders: { user1: 2, user2: 10 } },
@@ -26,7 +26,7 @@ let ORDERBOOK = {
 };
 
 
-let STOCK_BALANCES= {
+let STOCK_BALANCES : any= {
   user1: {
     BTC_USDT_10_Oct_2024_9_30: {
       yes: { quantity: 1, locked: 0 },
@@ -57,6 +57,35 @@ if (foundUser) {
   res.status(404).send('User not found');
 }
   
+});
+
+app.post('/order/no', (req: Request, res: Response) => {
+  const { userId, stockSymbol, quantity, price } = req.body;
+
+  if (STOCK_BALANCES[userId] && STOCK_BALANCES[userId][stockSymbol]) {
+    const stockBalance = STOCK_BALANCES[userId][stockSymbol];
+
+    if (stockBalance.quantity >= quantity) {
+      stockBalance.quantity -= quantity;
+      stockBalance.locked += quantity;
+
+      if (!ORDERBOOK[stockSymbol]) {
+        ORDERBOOK[stockSymbol] = { yes: {}, no: {} } as StockOrderBook;
+      }
+      if (!ORDERBOOK[stockSymbol].no[price]) {
+        ORDERBOOK[stockSymbol].no[price] = { total: 0, orders: {} };
+      }
+      ORDERBOOK[stockSymbol].no[price].total += quantity;
+      ORDERBOOK[stockSymbol].no[price].orders[userId] =
+        (ORDERBOOK[stockSymbol].no[price].orders[userId] || 0) + quantity;
+
+      res.json({ message: 'Sell order placed successfully' });
+    } else {
+      res.status(400).send('Insufficient stock quantity');
+    }
+  } else {
+    res.status(404).send('User not found or insufficient stock');
+  }
 });
 
 app.listen(port, () => {
